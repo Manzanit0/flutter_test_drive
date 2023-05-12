adf471cd83a5f5b26c73a72532f71fbb01697332
Javier García <unico.javier@gmail.com>
2023-05-12 09:09:56 +0200

    tabs for description

--- lib/pages/show_recipe.dart
+++ lib/pages/show_recipe.dart
@@ -10,64 +10,91 @@ class ShowRecipePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          recipe.name!,
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              title: Text(recipe.name!),
+              backgroundColor: theme.backgroundColor,
+              pinned: true,
+              floating: true,
+              bottom: TabBar(
+                isScrollable: true,
+                tabs: [
+                  Tab(
+                    child: Text('Ingredients'),
+                  ),
+                  Tab(child: Text('Steps')),
+                ],
+              ),
+            ),
+          ];
+        },
+        body: TabBarView(
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height,
+              color: theme.colorScheme.primaryContainer,
+              child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: SingleChildScrollView(
+                    child: Column(children: [
+                      ListTile(
+                          leading: Icon(Icons.share),
+                          title: Text("External URL"),
+                          subtitle: Text(recipe.externalUrl ?? "...")),
+                      ExpansionTile(
+                        title: Text(
+                          "Categories",
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic),
+                        ),
+                        children: <Widget>[
+                          Column(
+                            children: [
+                              for (Category content in recipe.categories!)
+                                ListTile(
+                                  title: Text(
+                                    "${content.master}: ${content.name!}",
+                                  ),
+                                  leading: Icon(Icons.category),
+                                )
+                            ],
+                          ),
+                        ],
+                      ),
+                      ExpansionTile(
+                        title: Text(
+                          "Ingredients",
+                          style: TextStyle(
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.italic),
+                        ),
+                        children: <Widget>[
+                          Column(
+                            children: _buildExpandableIngredients(recipe),
+                          ),
+                        ],
+                      ),
+                    ]),
+                  )),
+            ),
+            Container(
+                height: MediaQuery.of(context).size.height,
+                color: theme.colorScheme.primaryContainer,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(recipe.description ?? "No steps available"),
+                )),
+          ],
         ),
-        backgroundColor: theme.colorScheme.primary,
-      ),
-      // deluge-citadel-lethargic
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        color: theme.colorScheme.primaryContainer,
-        child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: SingleChildScrollView(
-              child: Column(children: [
-                ListTile(
-                    leading: Icon(Icons.share),
-                    title: Text("External URL"),
-                    subtitle: Text(recipe.externalUrl!)),
-                ExpansionTile(
-                  title: Text(
-                    "Categories",
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic),
-                  ),
-                  children: <Widget>[
-                    Column(
-                      children: [
-                        for (Category content in recipe.categories!)
-                          ListTile(
-                            title: Text(
-                              "${content.master}: ${content.name!}",
-                            ),
-                            leading: Icon(Icons.category),
-                          )
-                      ],
-                    ),
-                  ],
-                ),
-                ExpansionTile(
-                  title: Text(
-                    "Ingredients",
-                    style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic),
-                  ),
-                  children: <Widget>[
-                    Column(
-                      children: _buildExpandableIngredients(recipe),
-                    ),
-                  ],
-                ),
-              ]),
-            )),
-      ),
+      )),
     );
   }
 
